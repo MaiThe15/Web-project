e6c1f12768226df4861e27a11494a4f0806e26a5
MaiThe15 <maithek36@gmail.com>
2025-12-22 17:23:53 +0700

    change DB

--- database/init.sql
+++ database/init.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    role VARCHAR(20) DEFAULT 'client',
+    role VARCHAR(20) DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
